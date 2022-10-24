b931dd9a6957f4cef7583c97e3cb264c1ad848fc
HP <HP@DESKTOP-E26D1Q6>
2022-10-24 11:27:14 +0100

    Could not clone the openclassrooms P2C3 project as advised,given the original project was not created with eclipse but intelliJ. So I created a project from start.spring.io and am just pushing it to githup sothat I can end the course.

---
+++ src/main/java/com/oc/microservices/msa/ConstruisezDesMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.oc.microservices.msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstruisezDesMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstruisezDesMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oc/microservices/msa/ConstruisezDesMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc.microservices.msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstruisezDesMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
